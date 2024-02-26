dcedca0add9bad93505104db76716e57d880fbd3
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-02-26 18:13:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inlämningsuppgifterkapitel6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(största(200, 201));
+        }
+
+        static int största(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+
+            else if (tal1 < tal2)
+            {
+                return tal2;
+            }
+
+            else
+            {
+                return tal1;
+            }
+        }
+
+    }
+}
